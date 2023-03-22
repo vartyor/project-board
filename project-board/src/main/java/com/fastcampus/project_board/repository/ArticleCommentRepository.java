a5d9977bc9318d9879fef081c0fa4ba274986f78
vartyor <varute1997@gmail.com>
2023-03-22 19:41:42 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslPredicateExecutor',
    'QureydslBinderCustomizer'를 이용하여 간단하게 개발

--- project-board/src/main/java/com/fastcampus/project_board/repository/ArticleCommentRepository.java
+++ project-board/src/main/java/com/fastcampus/project_board/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.fastcampus.project_board.repository;
 
 import com.fastcampus.project_board.domain.ArticleComment;
+import com.fastcampus.project_board.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>, // 기본 검색 구현(부분 검색 및 대소문자 구분X)
+        QuerydslBinderCustomizer<QArticleComment>   // 입맛에 맞는 검색 기능 구현을 위해 사용
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true); // 리스트화하지 않은 porperty는 검색에서 제외(true)
+        bindings.including(root.content, root.createdAt, root.createdBy);// 원하는 필드 추가
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);                // 시, 분, 초까지 모두 동일하게 검색해야 한다.
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+    }
 }
--- project-board/src/main/java/com/fastcampus/project_board/repository/ArticleRepository.java
+++ project-board/src/main/java/com/fastcampus/project_board/repository/ArticleRepository.java
@@ -1,9 +1,31 @@
 package com.fastcampus.project_board.repository;
 
 import com.fastcampus.project_board.domain.Article;
+import com.fastcampus.project_board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>  // QClass가 들어가게 설정되어 있다.
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.hashtag, root.content, root.createdAt, root.createdBy);
+        // argument를 하나만 받는다. containsIgnoreCase는 대소문자 구분을 안한다(아래 구문).
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);          // like '${v}', 수동으로 검색하고 싶을 때
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);      // like '%${v}%', 관련된 검색어 모두 출력할 때
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);                // 시, 분, 초까지 모두 동일하게 검색해야 한다.
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
