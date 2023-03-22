1031cd25f523181f6a00578b80b3c3099052c974
vartyor <varute1997@gmail.com>
2023-03-22 11:21:59 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없는 요소이므로 추출이 가능하다.
    
    '@MappedSuperclass' 이용하여 상속 방식으로 추출
    
    그 외 '@DateTimeFormat' 요소 추가 및 jpa 옵션 개선

---
+++ project-board/src/main/java/com/fastcampus/project_board/domain/AuditingFields.java
@@ -0,0 +1,41 @@
+package com.fastcampus.project_board.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) // 웹 화면에 보여줄 때, 웹 화면에서 파라미터를 받아 세팅할 때 parsing에 대한 rule 정의
+    @CreatedDate
+    @Column(nullable = false, updatable = false)    // update가 불가능하다.
+    private LocalDateTime createdAt; // 생성일시
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
+
+}
