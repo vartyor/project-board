ac16f97c3c2f2144d129bb392b08d37aff95eb24
vartyor <varute1997@gmail.com>
2023-03-23 17:09:03 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메서드로 테스트를 통과하게 열어주었다.
    뷰의 뼈대를 잡았다.
    테스트에서는  MediaType 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메서드 수정

--- project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[View][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,7 +29,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view => text/html
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // view => text/html
                 .andExpect(view().name("articles/index"))   // viewResolve할 이름 존재유무 테스트
                 .andExpect(model().attributeExists("articles")); // map에 "articles"라는 model attribute가 있는지 확인
     }
