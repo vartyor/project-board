78daf912afadc85bd621c4089c0e1230bcfeabd2
vartyor <varute1997@gmail.com>
2023-04-16 21:41:48 +0900

    #28 - 게시글 페이지네이션 구현
    `이전 글`, `다음 글`만 구현하므로 게시판에 비해 단순하지만
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서
    타임리프 문법이 복잡해짐
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 사용

--- project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
@@ -91,12 +91,14 @@ class ArticleControllerTest {
         then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
-    @DisplayName("[View][GET] 게시글 리스트 상세 페이지 - 정상 호출")
+    @DisplayName("[View][GET] 게시글 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // Given
         Long articleId = 1L;
+        long totalCount = 1L;
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
+        given(articleService.getArticleCount()).willReturn(totalCount);
 
         // When & Then
         mvc.perform(get("/articles/1"))
@@ -104,7 +106,9 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attribute("totalCount", totalCount));
         then(articleService).should().getArticle(articleId);
     }
 
