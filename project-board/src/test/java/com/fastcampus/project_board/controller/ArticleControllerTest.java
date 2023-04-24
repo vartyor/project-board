408732dab96e239b0f5c4173cca5e237e12a19d1
vartyor <varute1997@gmail.com>
2023-04-24 15:38:13 +0900

    #28 - 검색 기능을 위해 SearchType 옵션 전달, 테스트
    게시판 페이지의 검색바에서 검색 종류를 드롭 다운 메뉴로 제공할 때, 해당 값을 뷰가 알게 하지 않고 서버로부터 받아서 쓰게 만들기 위해 
    값을 반환하는 로직을 테스트와 함께 추가

--- project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/project_board/controller/ArticleControllerTest.java
@@ -3,6 +3,7 @@ package com.fastcampus.project_board.controller;
 import com.fastcampus.project_board.DTO.ArticleWithCommentsDto;
 import com.fastcampus.project_board.DTO.UserAccountDto;
 import com.fastcampus.project_board.config.SecurityConfig;
+import com.fastcampus.project_board.domain.type.SearchType;
 import com.fastcampus.project_board.service.ArticleService;
 import com.fastcampus.project_board.service.PaginationService;
 import org.junit.jupiter.api.Disabled;
@@ -56,8 +57,33 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("paginationBarNumbers"));
+                .andExpect(model().attributeExists("paginationBarNumbers"))
+                .andExpect(model().attributeExists("searchTypes"));
         then(articleService).should().searchArticles(eq(null),eq(null),any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+    @DisplayName("[View][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void givenSearchKeyword_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue),any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+
+        // When & Then
+        mvc.perform(
+                get("/articles")
+                        .queryParam("searchType", searchType.name())
+                        .queryParam("searchValue", searchValue)
+                )
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(searchType),eq(searchValue),any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
